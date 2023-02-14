11c4971f9b09e6acd29095765060171d90b001a0
y-00jin <81798918+y-00jin@users.noreply.github.com>
2023-02-14 20:28:26 +0900

    유진

---
+++ drdr/src/pages/AddTree.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import '../resource/css/tree/AddTree.css';
+
+function AddTree() {
+  return (
+    <>
+      <div className='treeTop'>
+        <br></br>
+      </div>
+
+
+      <div className='treeCenter'>
+        <div className='treeHeader'>
+
+            <h3>도란도란</h3>
+        </div>
+        
+      </div>
+
+      <div className='treeBottom'>
+        <br></br>
+      </div>
+    </>
+  );
+}
+
+export default AddTree;
