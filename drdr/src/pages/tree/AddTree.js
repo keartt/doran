b2a24fbde57d4df34b71650b7a434ba7d94fb77e
y-00jin <81798918+y-00jin@users.noreply.github.com>
2023-02-15 21:19:24 +0900

    롤링페이퍼 생성 페이지
    롤링페이퍼 생성 페이지

--- drdr/src/pages/tree/AddTree.js
+++ drdr/src/pages/tree/AddTree.js
@@ -3,8 +3,8 @@ import '../../resource/css/tree/AddTree.css';
 import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
-
-
+import SearchIcon from '@mui/icons-material/Search';
+import {IconButton} from '@mui/material'
 import Header from '../fragment/header';
 
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
@@ -18,18 +18,30 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 import { FixedSizeList } from 'react-window';
 
+
+var arr = ['이기춘','강성현','김영재','이유진', '김상명'];
 function renderRow(props) {
   const { index, style } = props;
 
   return (
-    <ListItem style={style} key={index} component="div" disablePadding>
+    <ListItem style={style} key={arr[index]} value={arr[index]} component="div" disablePadding onClick={() => clicks(index)}>
       <ListItemButton>
-        <ListItemText primary={`Item ${index + 1}`} />
+        <ListItemText primary={`Item : ${arr[index]}`} />
+        <ListItemText primary={`부서`} />
+        <ListItemText primary={`직급`} />
+        <ListItemText primary={`아이디`} />
       </ListItemButton>
     </ListItem>
   );
 }
 
+function clicks(index){
+  var name = document.getElementsByName('searchName');
+  
+  name[0].defaultValue = " ";
+  //name[0].value = arr[index];
+  alert(arr[index]);
+}
 
 function AddTree() {
 
@@ -43,12 +55,16 @@ function AddTree() {
       <div className='treeCenter'>
 
         <Header />
+
+
         <form>
           <div className='treeMain'>
             <Grid container spacing={2}>
-
               <Grid item xs={12}>
-                <TextField required id="standard-required" fullWidth sx={{ m: 1 }} label="제목" defaultValue="" variant="standard" />
+                <TextField required id="standard-required" fullWidth sx={{
+                m: 1, '& .MuiInput-underline:after': { borderBottomColor: '#FF8000' }, "& label.Mui-focused": {
+                  color: '#FF8000;'
+                }}} label="제목" defaultValue="" variant="standard" />
               </Grid>
 
 
@@ -62,42 +78,65 @@ function AddTree() {
                     onChange={(newValue) => {
                       setValue(newValue);
                     }}
-                    renderInput={(params) => <TextField required id="standard-required" variant="standard" fullWidth sx={{ m: 1 }} {...params} />}
+                    renderInput={(params) => <TextField required id="standard-required" variant="standard" fullWidth sx={{
+                      m: 1, '& .MuiInput-underline:after': { borderBottomColor: '#FF8000' }, "& label.Mui-focused": {
+                        color: '#FF8000;'
+                      }}} {...params} />}
                   />
                 </LocalizationProvider>
               </Grid>
 
 
-              <Grid item xs={9}>
-                <TextField required id="standard-required" fullWidth sx={{ m: 1 }} label="수신인" defaultValue="" variant="standard" />
+              <Grid item xs={11}>
+                <TextField required id="standard-required" name="searchName" fullWidth sx={{
+                m: 1, '& .MuiInput-underline:after': { borderBottomColor: '#FF8000' }, "& label.Mui-focused": {
+                  color: '#FF8000;'
+                }}} label="수신인" defaultValue="" variant="standard" />
               </Grid>
 
-              <Grid item xs={3}>
+              <Grid item xs={1}>
 
-                <Button variant="contained" id="btnSearch" size="small">
-                  검색
-                </Button>
+                <IconButton variant="contained" id="btnSearch" size="small">
+                  <SearchIcon/>
+                </IconButton>
               </Grid>
 
 
+              <Grid item xs={12}>
 
-
-
-            </Grid>
-
-            <Box
-              sx={{ width: '100%', height: 400, maxWidth: 360, bgcolor: 'background.paper' }}
-            >
               <FixedSizeList
-                height={400}
-                width={360}
+                height={150}
                 itemSize={46}
-                itemCount={200}
+                itemCount={arr.length}
                 overscanCount={5}
               >
                 {renderRow}
               </FixedSizeList>
-            </Box>
+
+
+
+              </Grid>
+
+
+              <Grid item xs={12}>
+
+                <TextField id="standard-required" label="상세정보" fullWidth sx={{
+                m: 1, '& .MuiInput-underline:after': { borderBottomColor: '#FF8000' }, "& label.Mui-focused": {
+                  color: '#FF8000;'
+                }}}  multiline rows={5}  variant="standard" />
+              </Grid>
+
+              <Grid item xs={12}></Grid>
+              <Grid item xs={12}>
+                <Button variant="contained" id="btnCreate" size="small">
+                  편지 생성
+                </Button>
+              </Grid>
+
+
+            </Grid>
+
+            
 
 
 
